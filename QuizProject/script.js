1a167f156aeeea3f829eb379bcb86ca425bee807
Landon <norogoth@gmail.com>
2020-12-22 14:19:25 -0800

    Committing all my junk.

--- QuizProject/script.js
+++ QuizProject/script.js
@@ -18,10 +18,13 @@ const quizData = [
     }
 ];
 
+const question = document.getElementById('question');
 const a_text = document.getElementById('a_text');
 const b_text = document.getElementById('b_text');
 const c_text = document.getElementById('c_text');
 const d_text = document.getElementById('d_text');
+const submitButton = document.getElementById('submitButton');
+const answerChoices = document.getElementsByName('answer');
 
 let currentQuestion = 0;
 
@@ -29,6 +32,28 @@ loadQuiz();
 
 function loadQuiz(){
     const currentQuizData = quizData[currentQuestion];
-    question
-    currentQuestion++;
+    question.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
 }
+
+function getSelectedAnswer(){
+    for (var i = 0; length = answerChoices.length; i++){
+
+    }
+};
+
+submitButton.addEventListener("click", () => {
+    if (answerChoices){
+
+    }
+    if (quizData.length - 1 > currentQuestion){
+        currentQuestion++;
+        loadQuiz();
+    }
+    else{
+        window.location.assign("./results.html");
+    }
+});
